3db7f082adc94f951be4dbd4d4001633aaca089c
Caspers QuadMBP <casper.schipper@gmail.com>
2021-01-01 00:35:25 +0100

    warp

--- src/Main.elm
+++ src/Main.elm
@@ -12,10 +12,139 @@ module Main exposing
     , zero
     )
 
+import Browser
+import Browser.Dom as Dom
 import Hex
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Html.Events as Events
+import Json.Decode as D exposing (Decoder)
 import Maybe.Extra as M
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Task
+import Time
+
+
+main : Platform.Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+subscriptions _ =
+    Time.every 100 Tick
+
+
+init _ =
+    ( Model 0 0 0 0 0, Task.perform GotViewPort Dom.getViewport )
+
+
+type Msg
+    = MouseClick MouseClickData
+    | Touch TouchData
+    | GotViewPort Dom.Viewport
+    | Tick Time.Posix
+
+
+type alias Model =
+    { x : Int
+    , y : Int
+    , screenw : Int
+    , screenh : Int
+    , t : Float
+    }
+
+
+type alias MouseClickData =
+    { mouseButton : Int
+    , x : Int
+    , y : Int
+    }
+
+
+type alias TouchData =
+    { x : Int
+    , y : Int
+    }
+
+
+decodeMousePos : Decoder MouseClickData
+decodeMousePos =
+    D.map3 MouseClickData
+        (D.field "which" D.int)
+        (D.field "pageX" D.int)
+        (D.field "pageY" D.int)
+
+
+decodeTouch : Decoder TouchData
+decodeTouch =
+    D.map2 TouchData
+        (D.field "pageX" D.int)
+        (D.field "pageY" D.int)
+
+
+updateMaybe : a -> Maybe a -> a
+updateMaybe old mNew =
+    case mNew of
+        Just new ->
+            new
+
+        Nothing ->
+            old
+
+
+getLeftMousePos : MouseClickData -> Maybe ( Int, Int )
+getLeftMousePos data =
+    case data.mouseButton of
+        1 ->
+            Just ( data.x, data.y )
+
+        _ ->
+            Nothing
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MouseClick data ->
+            let
+                mPos =
+                    getLeftMousePos data
+            in
+            case mPos of
+                Just ( x, y ) ->
+                    ( { model | x = x, y = y }, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+        Touch data ->
+            ( { model
+                | x = data.x
+                , y = data.y
+              }
+            , Cmd.none
+            )
+
+        GotViewPort prt ->
+            ( { model | screenw = prt.viewport.width |> floor, screenh = prt.viewport.height |> floor }, Cmd.none )
+
+        Tick _ ->
+            ( { model
+                | t =
+                    if model.t > toFloat model.screenw then
+                        100
+
+                    else
+                        model.t + 5.0
+              }
+            , Cmd.none
+            )
 
 
 dash =
@@ -98,6 +227,75 @@ timeInterval steps =
     aux 0.0
 
 
+timeSlice : Int -> Float -> Float -> List Time
+timeSlice steps slice offs =
+    let
+        step =
+            slice / toFloat steps
+
+        aux curr =
+            if curr <= slice then
+                Time (curr + offs) :: aux (curr + step)
+
+            else
+                []
+    in
+    aux 0.0
+
+
+linlin : Float -> Float -> Float -> Float -> Float -> Float
+linlin a b c d x =
+    ((x - b) / (b - a)) * (d - c) + c
+
+
+timeWarp : Float -> List Time -> List Time
+timeWarp power lst =
+    let
+        firstLast =
+            case lst of
+                (Time first) :: ts ->
+                    case List.reverse ts of
+                        (Time last) :: _ ->
+                            Just ( first, last )
+
+                        _ ->
+                            Nothing
+
+                _ ->
+                    Nothing
+    in
+    case firstLast of
+        Just ( first, last ) ->
+            lst
+                |> List.map
+                    (\(Time x) ->
+                        x
+                            |> linlin first last 0 1
+                            |> (^) power
+                            |> linlin 0 1 first last
+                            |> Time
+                    )
+
+        Nothing ->
+            lst
+
+
+timeExtraval : Int -> Float -> List Time
+timeExtraval steps max =
+    let
+        step =
+            1.0 / toFloat steps
+
+        aux curr =
+            if curr <= max then
+                Time curr :: aux (curr + step)
+
+            else
+                []
+    in
+    aux 0.0
+
+
 interpolate : Float -> Float -> Time -> Float
 interpolate x1 x2 (Time t) =
     x1 + ((x2 - x1) * t)
@@ -132,19 +330,16 @@ offset xOffset yOffset =
     List.map (\( x, y ) -> ( x + xOffset, y + yOffset ))
 
 
-blackPolyline : Int -> List ( Int, Int ) -> Svg never
-blackPolyline alpha lst =
-    polyline [ strokeWidth "8", fill "none", "black" |> stroke, points (lst |> toPoints) ] []
+blackPolyline : Int -> Float -> List ( Int, Int ) -> Svg never
+blackPolyline width opacity lst =
+    polyline [ strokeWidth (String.fromInt width), fill "none", "black" |> stroke, strokeOpacity (String.fromFloat opacity), points (lst |> toPoints) ] []
 
 
-morph : List ( Int, Int ) -> List ( Int, Int ) -> Int -> List (List ( Int, Int ))
-morph lstA lstB steps =
+morph : List ( Int, Int ) -> List ( Int, Int ) -> List Time -> List (List ( Int, Int ))
+morph lstA lstB ts =
     let
-        map =
-            List.map
-
-        ts =
-            timeInterval steps
+        ( map, take ) =
+            ( List.map, List.take )
 
         ( lstAf, lstBf ) =
             ( map floatify lstA, map floatify lstB )
@@ -215,18 +410,42 @@ zeroPad str =
             str
 
 
-sine : List Time -> List Float
-sine ts =
-    List.map (\(Time t) -> (0.5 * sin (t * 3.14159265359)) + 1.0) ts
+sine : Float -> List Time -> List Float
+sine cycles ts =
+    List.map (\(Time t) -> sin (t * 3.14159265359 * cycles)) ts
 
 
-main =
+triangle : Float -> Float
+triangle x =
+    (x * 2.0)
+        |> (\a ->
+                if a > 1.0 then
+                    1.0 - (a - 1.0)
+
+                else
+                    a
+           )
+
+
+layer x y w h lineWidth =
     let
+        xf =
+            x / toFloat w
+
+        yf =
+            toFloat y / toFloat h
+
         num =
-            12
+            (w // 4) - (x / 4 |> round)
 
         ts =
-            timeInterval num
+            timeSlice num 1.0 xf
+
+        rev b =
+            1.0 - b
+
+        tslice =
+            timeSlice num 1.0 0.0 |> timeWarp (xf |> triangle |> (*) 2.0)
 
         twoString =
             toPoints two
@@ -234,8 +453,6 @@ main =
         ( map, concatMap ) =
             ( List.map, List.concatMap )
 
-        -- morphed =
-        --     morph three zero 32 |> map (offset 100 100) |> map blackPolyline
         oud =
             "-31-12-2020" |> fromString
 
@@ -246,23 +463,49 @@ main =
             offsets (List.length oud) 400 100
 
         perspective =
-            line ( 100, 0 ) ( 100, 3000 ) ts
+            line ( w // 2, h // 2 ) ( xf |> rev |> (*) (toFloat w * 10) |> round, xf |> rev |> (*) (toFloat h * 10) |> round ) ts
 
         trans =
-            List.repeat num 255
+            List.repeat num 0.8
 
         drawChar charA charB off transparancy =
-            morph charA charB num
+            morph charA charB ts
                 |> lineOffsetShapes perspective
-                |> map ((\( x, y ) -> offset x y) off)
-                |> map (blackPolyline transparancy)
+                |> map ((\( xx, yy ) -> offset xx yy) off)
+                |> map (blackPolyline lineWidth transparancy)
 
         allChars =
             List.map4 drawChar oud nieuw charOffsets trans |> List.concat
     in
-    svg
-        [ width "1000"
-        , height "1000"
-        , viewBox "0 0 9000 5000"
+    allChars
+
+
+view : Model -> Html Msg
+view model =
+    let
+        ( x, y ) =
+            ( model.x, model.y )
+
+        w =
+            model.screenw
+
+        h =
+            model.screenh
+    in
+    Html.div
+        [ Events.on "mousemove" (D.map MouseClick decodeMousePos)
+        , Events.on "touchmove" (D.map Touch decodeTouch)
         ]
-        allChars
+        [ Html.p []
+            [ Html.text <|
+                (x |> String.fromInt)
+                    ++ " "
+                    ++ String.fromInt y
+            ]
+        , svg
+            [ width (String.fromInt w)
+            , height (String.fromInt h)
+            , viewBox "0 0 10000 5000"
+            ]
+            (layer model.t y w h 4)
+        ]
